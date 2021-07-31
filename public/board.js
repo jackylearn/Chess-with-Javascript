d6667d5f1c3c28cc6dc6a980ea277da2efede0e4
Jacky <jacky3627jp@gmail.com>
2021-08-01 02:55:29 +0800

    swap sidebar legend, modify CSS

--- public/board.js
+++ public/board.js
@@ -1,24 +1,36 @@
 const board = document.getElementsByClassName('board')[0];
-const boardLetters = document.getElementsByClassName('boardLetters')[0];
-const boardNumbers = document.getElementsByClassName('boardNumbers')[0];
+const rankLegend_left = document.getElementsByClassName('rankLegend-left')[0];
+const rankLegend_right = document.getElementsByClassName('rankLegend-right')[0];
+const fileLegend_top = document.getElementsByClassName('fileLegend-top')[0];
+const fileLegend_bottom = document.getElementsByClassName('fileLegend-bottom')[0];
 
 const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
 let black = false;
 
-for (let i = 0; i < 8; i++) {
-    let letter = document.createElement('div')
-    letter.textContent = letters[i];
-    boardLetters.appendChild(letter);
+for (let i = 1; i <= 8; i++) {
+    let rank1 = document.createElement('div')
+    rank1.classList.add('sidebar')
+    rank1.textContent = i;
+    rankLegend_left.appendChild(rank1);
+    let rank2 = document.createElement('div')
+    rank2.classList.add('sidebar')
+    rank2.textContent = i;
+    rankLegend_right.appendChild(rank2);
 
-    let number = document.createElement('div')
-    number.textContent = i + 1;
-    boardNumbers.appendChild(number);
+    let file1 = document.createElement('div')
+    file1.textContent = letters[i-1];
+    file1.classList.add('sidebar')
+    fileLegend_top.appendChild(file1);
+    let file2 = document.createElement('div')
+    file2.classList.add('sidebar')
+    file2.textContent = letters[i-1];
+    fileLegend_bottom.appendChild(file2);
 }
 
 
 
-for (let i = 0; i < 64; i++) {
+for (let i = 1; i <= 64; i++) {
     const square = document.createElement('div')
     if (black) {
         square.classList.add('black');
@@ -31,7 +43,7 @@ for (let i = 0; i < 64; i++) {
 
     black = !black;
 
-    if (i % 8 === 7) black = !black
+    if (i % 8 === 0) black = !black
     board.appendChild(square);
 
 }
